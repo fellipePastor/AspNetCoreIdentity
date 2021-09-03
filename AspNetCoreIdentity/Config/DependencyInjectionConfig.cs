40e536f85e54f1df7008a74c38448f3ab7cd258f
fellipePastor <fellipepastorp@gmail.com>
2021-09-03 18:31:23 -0300

    Add project files.

---
+++ AspNetCoreIdentity/Config/DependencyInjectionConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentity.Config
+{
+    public static class DependencyInjectionConfig
+    {
+
+        public static IServiceCollection AddAuthorizationConfig(this IServiceCollection services) 
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("PodeExcluir", policy => policy.RequireClaim("PodeExcluir"));
+
+            });
+
+
+            return services;
+        }
+
+    }
+}
---
+++ AspNetCoreIdentity/Startup.cs
@@ -0,0 +1,84 @@
+using AspNetCoreIdentity.Areas.Identity.Data;
+using AspNetCoreIdentity.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCoreIdentity
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment hostingEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                              .SetBasePath(hostingEnvironment.ContentRootPath)
+                              .AddJsonFile("appsettings.json", true, true)
+                              .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true, true)
+                              .AddEnvironmentVariables();
+            
+            Configuration = builder.Build();
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region Config 
+            services.AddControllersWithViews();
+
+            services.AddDbContext<AspNetCoreIdentityContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("AspNetCoreIdentityContextConnection")));
+
+            #endregion
+
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AspNetCoreIdentityContext>();
+
+            services.AddAuthorizationConfig();
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
